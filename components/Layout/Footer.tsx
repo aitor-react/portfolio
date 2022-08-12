9211a3bcabf0ec759a9822f4464f2f687bf0f811
aitor <penguinft1@gmail.com>
2022-08-12 13:10:28 +0200

    Form set up

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,17 +1,49 @@
-import React from "react";
-import IconLink from "../core/IconLink";
-import Title from "../core/Title";
+import {useState} from "react";
+
 
 function Footer() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
+
+    const handleSubmit = (e: { preventDefault: () => void; }) => {
+        const data = {
+            name,
+            email,
+            message
+        }
+        console.log(data)
+    }
+
   return (
-    <footer className='py-12 flex items-center justify-between' >
-      <Title title="Contact" />
-      <div className="flex space-x-4">
-        {/*<IconLink label="Github" href="https://github.com/aitor-react"/>*/}
-        {/*<IconLink label="Linkedin" href="https://www.linkedin.com/in/aitoralonsocantero/"/>*/}
-      </div>
-    </footer >
-  );
+    <div>
+        <form onSubmit={handleSubmit} className="">
+          <label htmlFor="name">Name:</label>
+          <input
+            id="name"
+            type="text"
+            onChange={e => setName(e.target.value)}
+            required
+          />
+          <label htmlFor="email">Email:</label>
+          <input
+            id="email"
+            type="email"
+            onChange={e => setEmail(e.target.value)}
+            required
+          />
+          <label htmlFor="message">Message:</label>
+          <textarea
+            id="message"
+            type="textarea"
+            rows="3"
+            onChange={e=> setMessage(e.target.value)}
+            required
+          />
+          <button type="submit">Send</button>
+        </form>
+    </div>
+  )
 }
 
 export default Footer;
