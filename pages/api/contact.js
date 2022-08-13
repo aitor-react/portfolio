f278d13174c3f9292deb70ccdfcb2fb8e5680d84
aitor <penguinft1@gmail.com>
2022-08-13 16:42:44 +0200

    SendGrid mail contact api

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,7 +1,25 @@
+const mail = require('@sendgrid/mail');
 
+mail.setApiKey(process.env.API_KEY);
 
-export default (req, res) => {
+export default async (req, res) => {
   const body = JSON.parse(req.body);
-  console.log(body);
+
+  const message = `
+    Name: ${body.name}\r\n
+    Email: ${body.email}\r\n
+    Message: ${body.message}
+  `;
+
+  const data = {
+    to: 'SENDER_EMAIL',
+    from: 'RECEIVER_EMAIL',
+    subject: `New message from ${body.name}`,
+    text: message,
+    html: message.replace(/\r\n/g, '<br />'),
+  };
+
+  await mail.send(data);
+
   res.status(200).json({ status: 'OK' });
 };
