cfd1801774165f045da443e4a7967fbe850fc6ec
aitor <penguinft1@gmail.com>
2022-08-16 16:31:41 +0200

    add quizz

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,13 +7,14 @@ import HeadingWithText from "../components/Layout/HeadingWithText";
 import whitelist from '/public/whitelist.png'
 import mynft from '/public/mynft.png' 
 import countries from '/public/countries.png' 
+import quizz from '/public/quizz.png'
 
 import Button from "../components/core/Button";
 
 
 const Home: NextPage = () => {
   return (
-    <div className='min-h-screen'>
+    <>
       <Head>
         <title>{"Aitor's Portfolio"}</title>
       </Head>
@@ -22,10 +23,8 @@ const Home: NextPage = () => {
           <HeadingWithText
             title="Hi! I'm Aitor,"
             style="gradient"
-            text="I'm a self-taught Frontend Developer based in Prague, CZ, originally from Spain. Apart from web2 apps I have a very fond interest in Web3 and decentralized applications built using the Ethereum Virtual Machine."
-            //label="Redirect to contact form in page"
+            text="I'm a self-taught Frontend Developer based in Prague, Czech Republic, originally from Spain. Apart from web2 apps I'm very passionate about Web3 and decentralized applications built using the Ethereum Virtual Machine. Feel free lo look around and check some of the highlighted projects." 
           />
-          
         </section>
         {/* Projects Section */ }
         <section className="py-12">
@@ -117,9 +116,9 @@ const Home: NextPage = () => {
             className="flex flex-col space-y-8 mt-4 md:flex-row-reverse md:justify-between md:items-center md:space-y-0">
 
               {/*Image Container*/}
-              <a href="https://github.com/aitor-react/NFT-collection-whitelist" target='_blank' rel=" noopener noreferrer">
+              <a href="https://github.com/aitor-react/Quizz-game" target='_blank' rel=" noopener noreferrer">
                 <Image className="w-full h-[250px] md:w-[350px] md:h-[180px] bg-gradient-to-r from-cyan-300 to-blue-500 rounded-md shadow-lg md:ml-12" 
-                src={whitelist}
+                src={quizz}
                 alt='project'
                 priority
                 />
@@ -131,9 +130,9 @@ const Home: NextPage = () => {
                   2022
                 </span>
                 <HeadingWithText
-                  title="NFT Collection Whitelist"
-                  text="This site allows the first 10 users to enter a whitelist. It's deployed in the Goerli testnet which is needed to interact with the contract"
-                  label="Built using Hardhat, React, Ethers, Next"
+                  title="Quizz game"
+                  text="Try score the highest points in the quizz. Even if you miss you still have the chance to learn where your mistakes were made!"
+                  label="Built using React"
                 />
               </div>
             </div>
@@ -142,7 +141,7 @@ const Home: NextPage = () => {
           <Button lable="View all projects" />
         </section>
       </main>
-    </div>
+    </>
   );
 };
 
